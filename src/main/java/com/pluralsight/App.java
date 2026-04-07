fece75985a08ec90f42828e64d7c562ffbfe518f
tyrailrosser <rossertyrail3@gmail.com>
2026-04-07 11:10:33 -0400

    Added another sout

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -4,5 +4,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Testing to see if this network works");
         System.out.println("Hello world");
+        System.out.println("Yes this works");
+        System.out.println("I got it");
     }
 }
