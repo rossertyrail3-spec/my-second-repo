b73b6233ba5edd66f0b7d5bb0272dae38410cb45
tyrailrosser <rossertyrail3@gmail.com>
2026-04-07 11:07:32 -0400

    Added another sout

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -3,5 +3,6 @@ package com.pluralsight;
 public class App {
     public static void main(String[] args) {
         System.out.println("Testing to see if this network works");
+        System.out.println("Hello world");
     }
 }
